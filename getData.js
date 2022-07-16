9fa3d1037eccf6d548b81cc3d82189c402816ef6
reeshaa <shenoyreesha@gmail.com>
2022-07-16 15:44:13 +0530

    a

--- getData.js
+++ getData.js
@@ -1,5 +1,8 @@
 module.exports = async function (req, res, collection) {
-  let result= await collection.find({ hall: req.query.hall }).toArray();
+  var today = new Date().toLocaleDateString("en-US");
+  let result = await collection
+    .find({ hall: req.query.hall, date: { $gte: today } })
+    .toArray();
   console.log(result);
   return res.status(200).json(result);
 };
--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var mongo = require("mongodb").MongoClient;
 
-mongo.connect(
-  "mongodb+srv://reesha1234:reesha1234@cluster0.tcjqumw.mongodb.net/?retryWrites=true&w=majority",
-  (err, client) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("Connected to DB");
-    let db = client.db("hall");
-    let collection = db.collection("booking");
-
-    require("./server.js")(collection);
+mongo.connect(process.env.MONGO_URL, (err, client) => {
+  if (err) {
+    console.log(err);
+    return;
   }
-);
- 
+  console.log("Connected to DB");
+  let db = client.db("hall");
+  let collection = db.collection("booking");
+
+  require("./server.js")(collection);
+});
